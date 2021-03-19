2cf88e8193bb4fa5b6d153508162890573829741
DESKTOP-3EGOLRT\prave <praveen.p.r@hotmail.com>
2021-03-19 14:11:39 -0400

    Corrected type with modulo arithmetic

--- Algorithms/MinCostOfTickets/MinCostOfTickets_TabuOpt.cs
+++ Algorithms/MinCostOfTickets/MinCostOfTickets_TabuOpt.cs
@@ -1,20 +1,47 @@
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //case1
+        {
+            var sol = new Solution();
+            int[] days = new int[] { 1, 4, 6, 7, 8, 20 };
+            int[] costs = new int[] { 2, 7, 15 };
+            Console.WriteLine($"Input: [{string.Join(",", days)} ]");
+            var minCost = sol.MincostTickets(days, costs);
+            Console.WriteLine($"Expected: 11, Acutal:{minCost}");
+        }
+
+        //case2
+        {
+            var sol = new Solution();
+            int[] days = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31 };
+            int[] costs = new int[] { 2, 7, 15 };
+
+            var minCost = sol.MincostTickets(days, costs);
+            Console.WriteLine($"Expected: 17, Acutal:{minCost}");
+        }
+    }
+}
 public class Solution
 {
     /*
-     In this optimized version we use modulus arithmetic to keep a constant DP datastructure size = 31 (Largest pass duration + 1)
+     In this optimized version we use modulo arithmetic to keep a constant DP datastructure size = 31 (Largest pass duration + 1)
 
-       in modulus arithmetic: to look back certain number of days (upto 30 days) 
+       in modulo arithmetic: to look back certain number of days (upto 30 days) 
        we use the following formula: 
        prevDay = (dpLength + (currIndex - pass-duration)% dpLength) % dpLength
 
        explanation: 
-        in Modulus arithmetic adding dpLenth any number of times to a value and doing a mod % dpLength will keep the value to the corresponding index within the arary
+        in modulo arithmetic adding dpLenth any number of times to a value and doing a mod % dpLength will keep the value to the corresponding index within the arary
         -(currIndex - pass-duration)% dpLength might result in a negative index 
         -this index is gauranteed to be to between -dpLength and +dpLength as we do the mod with dpLength
         -now adding dpLength to the resulting value and taking the mod(val, dpLength) we get a value between 0 and dpLength
 
        Basic idea is we keep a fixed length array and overwrite elements when we run out of size of the array
-       Due to the modulus arithmetic we always overwrite the last item that is 32nd item with a new value
+       Due to the modulo arithmetic we always overwrite the last item that is 32nd item with a new value
 
     Pseudocode
     passes[] = {1, 7, 30}
@@ -54,7 +81,7 @@ public class Solution
             int min = int.MaxValue;
             for (int j = 0; j < passes.Length; j++)
             {
-                //find previous day's index using modulus arithmetic
+                //find previous day's index using modulo arithmetic
                 int prevDay = (dpLength + (i - passes[j]) % dpLength) % dpLength;
 
                 //Add current pass cost to the previous day's cost
