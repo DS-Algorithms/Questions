3dd81303d2d67b8d5ae6a4943c984a34a1749c48
DESKTOP-3EGOLRT\prave <praveen.p.r@hotmail.com>
2021-03-21 14:52:17 -0400

    Coinsum recursive without printing coins

--- Algorithms/DynamicProg/CoinSum/CoinSum_Recursion.cs
+++ Algorithms/DynamicProg/CoinSum/CoinSum_Recursion.cs
@@ -1,63 +1,4 @@
 using System;
-using System.Collections.Generic;
-
-public class Test
-{
-    public static void Main()
-    {
-        //Case 1
-        {
-
-            int[] coins = new int[] { 1, 2, 3 };
-            int total = 4;
-            Console.WriteLine($"Case1 input: [ {string.Join(", ", coins)} ], target:{total} ");
-            int expected = 4;
-            var actual = Compute.CoinSum(coins, total);
-            PrintArray(Compute._results);
-            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
-        }
-
-        // Case 2
-        {
-            int[] coins = new int[] { 8, 3, 1, 2 };
-            int total = 3;
-            Console.WriteLine($"Case2 input: [ {string.Join(", ", coins)} ], target:{total} ");
-            int expected = 3;
-            var actual = Compute.CoinSum(coins, total);
-            PrintArray(Compute._results);
-            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
-        }
-        // Case 3
-        {
-            int[] coins = new int[] { 2, 5, 3, 6 };
-            int total = 10;
-            int expected = 5;
-            Console.WriteLine($"Case3 input: [ {string.Join(", ", coins)} ], target:{total} ");
-            var actual = Compute.CoinSum(coins, total);
-            PrintArray(Compute._results);
-            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
-        }
-
-        // Case 4
-        {
-            int[] coins = new int[] { 2, 5, 3, 6 };
-            int total = 50;
-            // int expected = 5;
-            Console.WriteLine($"Case4 input: [ {string.Join(", ", coins)} ], target:{total} ");
-            var actual = Compute.CoinSum(coins, total);
-            PrintArray(Compute._results);
-            // Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual==expected}");  
-        }
-    }
-
-    private static void PrintArray(List<List<int>> array)
-    {
-        foreach (var item in array)
-        {
-            Console.WriteLine($"[ {string.Join(", ", item.ToArray())} ]");
-        }
-    }
-}
 
 /*
 Given an array of coins and a target total, return how many unique ways there are to use the coins to make up that total.
@@ -87,7 +28,7 @@ Diagram
 
  Approach 1
  ===========
- 
+ Complexity O(N^N)
  0 1 2
 [1,2,3]
 
@@ -115,8 +56,16 @@ Diagram
    2,1,1
    3,1
    
+   pseudo
+   ======
+   At each level iterate through all different coins
+   Generate all possible permutations (eg: 1,1,2; 1,2,1; 2, 1, 1)
+   Sort each combination and add to a hashtable
+    - so that there is only one entry for - 1,1,2; 1,2,1 or 2, 1, 1
+   
     Approach 2
    ===========
+   Complexity O(2^N)
  0 1 2
 [1,2,3]
 
@@ -142,8 +91,14 @@ pseudo
   if target < 0 || c < 0 return 0
   if target = 0 and c==0 return 1
 
+  // generate recursion branch when not using a coin
+   result1 = recurse(target, index-1) 
+  // generate recursion branch when using the coin and to include all possible combinations with that coin
+  // - note: we are not decrementing the index here
+  // - this will result in cases where the coin can be using any number of times until we hit the base case 
+  result2 = recurse(target-coin[index], index)
   
-  return recurse(target, index-1) + recurse(target-coin[index], index)
+  return result1 + result2
 */
 
 // Time Complexity:
@@ -152,27 +107,55 @@ public class Compute
 {
 
     private static int[] _coins;
-    public static List<List<int>> _results = new List<List<int>>();
     public static int CoinSum(int[] coins, int total)
     {
         _coins = coins;
 
-        return Traverse(total, _coins.Length - 1, new List<int>());
+        return Traverse(total, _coins.Length - 1);
     }
 
-    public static int Traverse(int total, int index, List<int> combination)
+    public static int Traverse(int total, int index)
     {
         if (total < 0 || index < 0) return 0;
-        if (total == 0)
-        {
-            _results.Add(combination);
-            return 1;
-        }
-        var notTakeList = new List<int>(combination);
-        var takeList = new List<int>(combination);
-        takeList.Add(_coins[index]);
-        return Traverse(total, index - 1, notTakeList) +
-            Traverse(total - _coins[index], index, takeList);
+        if (total == 0) return 1;
+
+        return Traverse(total, index - 1) +
+            Traverse(total - _coins[index], index);
     }
 }
 
+public class Test
+{
+    public static void Main()
+    {
+        //Case 1
+        {
+
+            int[] coins = new int[] { 1, 2, 3 };
+            int total = 4;
+            Console.WriteLine($"\nCase1 input: [ {string.Join(", ", coins)} ], target:{total} ");
+            int expected = 4;
+            var actual = Compute.CoinSum(coins, total);
+            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
+        }
+
+        // Case 2
+        {
+            int[] coins = new int[] { 8, 3, 1, 2 };
+            int total = 3;
+            Console.WriteLine($"\nCase2 input: [ {string.Join(", ", coins)} ], target:{total} ");
+            int expected = 3;
+            var actual = Compute.CoinSum(coins, total);
+            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
+        }
+        // Case 3
+        {
+            int[] coins = new int[] { 2, 5, 3, 6 };
+            int total = 10;
+            int expected = 5;
+            Console.WriteLine($"\nCase3 input: [ {string.Join(", ", coins)} ], target:{total} ");
+            var actual = Compute.CoinSum(coins, total);
+            Console.WriteLine($"Expected: {expected}, Actual: {actual}, Passed: {actual == expected}");
+        }
+    }
+}
